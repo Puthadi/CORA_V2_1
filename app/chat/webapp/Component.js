55f4c2cf49b7b6e061670e57807fff1cca1a28fc
Suman kumar puthadi <Suman.puthadi01@sap.com>
2026-05-22 11:48:52 +0530

    Fix app not loading: use core:HTML control and register device model

--- app/chat/webapp/Component.js
+++ app/chat/webapp/Component.js
@@ -1,11 +1,16 @@
 sap.ui.define([
-  "sap/ui/core/UIComponent"
-], function (UIComponent) {
+  "sap/ui/core/UIComponent",
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/Device"
+], function (UIComponent, JSONModel, Device) {
   "use strict";
   return UIComponent.extend("cc.errorresolution.chat.Component", {
     metadata: { manifest: "json" },
     init: function () {
       UIComponent.prototype.init.apply(this, arguments);
+      var oDeviceModel = new JSONModel(Device);
+      oDeviceModel.setDefaultBindingMode("OneWay");
+      this.setModel(oDeviceModel, "device");
     }
   });
 });
